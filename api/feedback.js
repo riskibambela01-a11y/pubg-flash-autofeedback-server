dee539556584c6e77dbff76911f266048ca758fa
Riski Bambela <riskibambela01@gmail.com>
2026-09-25 19:51:22 +0800

    Update feedback.js

--- api/feedback.js
+++ api/feedback.js
@@ -229,7 +229,7 @@ export default async function handler(req, res) {
     "╚═══━━━─── • ───━━━═══╝\n" +
     "🏆PUBG FLASH 🏆\n" +
     "🔥 AUTO FEEDBACK 🔥\n" +
-    "🦠 Bahan: AUTOFEEDBACK V9\n" +
+    "🦠 Bahan: AUTOFEEDBACK V1\n" +
     htmlEscape(pubgDisplay) +
     "\n" +
     "👤 Nickname: " +
